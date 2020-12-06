310c8187832eafc9c70c3c109cb3fea4c77f863b
Johann Zucco <jszucco@gmail.com>
2020-12-06 18:24:08 -0300

    Adicionada view de favoritos. Corrigidos bugs de layout

--- promocoes/forms.py
+++ promocoes/forms.py
@@ -3,15 +3,10 @@ from django.forms import ModelForm
 from .models import Promocao
 
 class PromocaoForm(ModelForm):
-    """
-    nome = forms.CharField(label='Nome', max_length=100)
-    cidade = forms.CharField(label='Cidade', max_length=100)
-    uf  = forms.CharField(label='UF', max_length=2)
-    email = forms.EmailField(label='E-mail', max_length=100)
-    """
     class Meta:
         model = Promocao
         fields = '__all__'
+        exclude = ['favoritos']
         #fields = ['nome', 'cidade', 'uf', 'email'] -> Se quiser usar campos específicos.
         #exclude = ['title'] -> Se quiser excluir algum campo
         #widgets = { 'produto': forms.TextInput(attrs={'class': 'CSSVaiAqui'}), }
@@ -20,4 +15,4 @@ class PromocaoForm(ModelForm):
 class PromocaoEditForm(ModelForm):
     class Meta:
         model = Promocao
-        exclude = ['produto', 'loja']
+        exclude = ['produto', 'loja', 'favoritos']
